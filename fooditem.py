bb4f16cef0a2f0903570e0993cbb2294352c9076
AnishaSabu <annrosepaul4@gmail.com>
2016-05-17 02:39:22 -0700

    program to read two food items and it's price and display these read values

---
+++ fooditem.py
@@ -0,0 +1,6 @@
+a=raw_input("enter the first food item: ") #enter first food item name#
+aprice=float(raw_input("enter price: ")) #enter its cost#
+b=raw_input("enter the second food item: ")#enter second food item name#
+bprice=float(raw_input("enter price: "))#enter its cost#
+print "food item:%s cost:%d"%(a,aprice) #printing  first food item and its cost#
+print "food item:%s cost:%d"%(b,bprice)#printing second food item and its cost#
