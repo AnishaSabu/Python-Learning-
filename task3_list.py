244cb6583bf27a15cd05dff4d1e21beed6a253d0
AnishaSabu <annrosepaul4@gmail.com>
2016-05-18 16:18:18 +0530

    program to temperature from and to farhenheit and celsius

---
+++ task3_list.py
@@ -0,0 +1,6 @@
+F1=int(raw_input("enter the temperature in fahrenheit: "))
+C1=(F1-32)*(5/9)
+print "temperature in celsius :%d"%(C1)
+C2=int(raw_input("enter the temperature in celsius: "))
+F2=(C2*(9/5)+32)
+print "temperature in celsius :%d"%(F2)
