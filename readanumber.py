fe1774495e3ab310ff7d8cc3ded23cc58fd7ba59
AnishaSabu <annrosepaul4@gmail.com>
2016-05-18 02:22:49 -0700

    program tp print the number in words

---
+++ readanumber.py
@@ -0,0 +1,21 @@
+num=input("enter a number from (0-9): ")
+if num==0:
+ print "number is ZERO"
+elif num==1:
+ print "number is ONE"
+elif num==2:
+ print "num is TWO"
+elif num==3:
+ print "number is THREE"
+elif num==4:
+ print "number is FOUR"
+elif num==5:
+ print "number is FIVE"
+elif num==6:
+ print "number is SIX"
+elif num==7:
+ print "number is SEVEN"
+elif num==8:
+ print "number is EIGHT"
+else:
+ print "number is NINE"
