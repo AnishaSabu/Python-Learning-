bad23c9136b1aa5a5161f305278e71a3f09fe715
AnishaSabu <annrosepaul4@gmail.com>
2016-05-21 22:44:22 +0530

    functions to add sub mult and divide two numbers

---
+++ calculator.py
@@ -0,0 +1,16 @@
+def div(a,b):
+ c=float(a)/float(b)
+ print "division : %f" %(c)
+ return 
+def mult(a,b):
+   c=a*b
+   print "product : ",c
+   return
+def add(a,b):
+    c=a+b
+    print "sum= ",c
+    return
+def sub(a,b):
+    c=int(a-b)
+    print "difference : ",c
+    return
