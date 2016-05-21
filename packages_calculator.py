c78bab1b8fc765d6b35c04b72c6f37350dfd5d25
AnishaSabu <annrosepaul4@gmail.com>
2016-05-21 23:09:57 +0530

    program to import other packages or modules to current module

---
+++ packages_calculator.py
@@ -0,0 +1,11 @@
+import summation
+import subtraction
+import multiplication
+import division
+a=int(raw_input("enter the first number : "))
+b=int(raw_input("enter the second number : "))
+summation.add(a,b)
+subtraction.sub(a,b)
+multiplication.mult(a,b)
+division.div(a,b)
+
