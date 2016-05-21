c40b92119f724e760bb80377371cc3d56c5382af
AnishaSabu <annrosepaul4@gmail.com>
2016-05-21 21:55:22 +0530

    program to print whole numbers upto 9

---
+++ whileelse.py
@@ -0,0 +1,6 @@
+num=0 #a value is assigned to variable num
+while num<10: #to print num unless it is equal tp greater then 10
+  print "num is %d"%(num)
+  num=num+1
+else:
+  print "num is not less than 10" #to print this msg when the value becomes equal to 10
