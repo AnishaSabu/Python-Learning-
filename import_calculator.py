348f86c2f5afc600506e081205870fa9135a2c19
AnishaSabu <annrosepaul4@gmail.com>
2016-05-21 22:45:22 +0530

    program to import the calculator module

---
+++ import_calculator.py
@@ -0,0 +1,7 @@
+import calculator
+a=int(raw_input("enter the  first number:"))
+b=int(raw_input("enter the second number:"))
+calculator.add(a,b)
+calculator.sub(a,b)
+calculator.mult(a,b)
+calculator.div(a,b)
